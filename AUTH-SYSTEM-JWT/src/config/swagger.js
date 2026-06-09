1baaebcb7596d80dd0c5ad05c9890a5b20e0e946
wara1314 <hawarahmadhani561@gmail.com>
2026-06-09 19:53:58 +0700

    fix: update swagger path and console log url

--- AUTH-SYSTEM-JWT/src/config/swagger.js
+++ AUTH-SYSTEM-JWT/src/config/swagger.js
@@ -1,5 +1,6 @@
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const path = require("path");
 
 const options = {
     definition: {
@@ -32,7 +33,7 @@ const swaggerSpec = swaggerJsdoc(options);
 const setupSwagger = (app) => {
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-console.log(` Swagger UI: http://localhost:${process.env.PORT ||3000}/api-docs`);
+console.log(` Swagger UI: http://railway.app`);
 };
 
 module.exports = { setupSwagger };
