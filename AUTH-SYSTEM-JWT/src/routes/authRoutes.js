ec6dab573d0605f55a567e246b4ebdfffffc938f
wara1314 <hawarahmadhani561@gmail.com>
2026-06-10 21:14:25 +0700

    fix

--- AUTH-SYSTEM-JWT/src/routes/authRoutes.js
+++ AUTH-SYSTEM-JWT/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authController'); 
 const { authenticate, authorize } = require('../middleware/authMiddleware');
 
 console.log("Cek fungsi authenticate:", typeof authenticate);
