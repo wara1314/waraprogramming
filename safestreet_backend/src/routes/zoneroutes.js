385eb4acc137c3d972ee54973e31468df2ab43bb
wara1314 <hawarahmadhani561@gmail.com>
2026-06-14 18:04:20 +0700

    fix

--- safestreet_backend/src/routes/zoneroutes.js
+++ safestreet_backend/src/routes/zoneroutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const prisma = require("../config/prisma");
-const { cekKunciToken } = require("../middlewares/authMiddleware");
+const { cekKunciToken } = require("../middlewares/authMiddlewares");
 const { aturanLaporJalan } = require("../validations/zoneValidation");
 
 /**
