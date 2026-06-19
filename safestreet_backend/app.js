d3c48c13677151df758d5ff9de1a66b55f916edb
wara1314 <hawarahmadhani561@gmail.com>
2026-06-19 20:39:39 +0700

    fix

--- safestreet_backend/app.js
+++ safestreet_backend/app.js
@@ -63,6 +63,6 @@ app.use((err, req, res, next) => {
     res.status(500).json({ success: false, message: "terjadi kesalahan internal pada server" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log("server aman berjalan di port", PORT);
 });
