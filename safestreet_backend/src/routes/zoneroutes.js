8ed03b4d9938698acc799df45b9038968e964591
wara1314 <hawarahmadhani561@gmail.com>
2026-06-21 14:24:53 +0700

    fix

--- safestreet_backend/src/routes/zoneroutes.js
+++ safestreet_backend/src/routes/zoneroutes.js
@@ -28,7 +28,47 @@ router.get("/", async (req, res, next) => {
 
 /**
  * @swagger
- * /api/zones:
+ * /api/zones/{id}:
+ * get:
+ *   tags: [Zones]
+ *   summary: Mendapatkan detail informasi dari satu Lokasi jalan rawan (ENDPOINT BARU 4)
+ *   parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *        description: ID nomor jalan rawan yang dicari
+ *   responses:
+ *      200:
+ *        description: Detail data jalan berhasil ditemukan
+ *      404:
+ *        description: Data jalan rawan tidak ditemukan
+ */
+router.get("/:id", async (req, res, next) =>{
+    try {
+        const { id } = req.params;
+        const detailJalan = await prisma.dangerZone.findUnique({
+            where: { id: parseInt(id) }
+        });
+
+        if (!detailjalan) {
+            return res.status(404).json({
+                success: false,
+                message: "Waduh, data jalan rawan tidak ditemukan!"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: detailJalan
+        });
+    } catch (error) { next(error); }
+});
+
+/**
+ * @swagger
+* /api/zones:
  *   post:
  *     tags: [Zones]
  *     summary: Warga melaporkan lokasi jalan rawan baru
@@ -128,6 +168,53 @@ router.patch("/:id", cekKunciToken, batasiHakAkses("PETUGAS"), async (req, res, 
             success: true,
             message: "Selamat petugas, status laporan jalan rawan berhasil diperbarui!",
             data: jalanDiperbarui
+        });
+    } catch (error) { next(error); }
+});
+
+/**
+ * @swagger
+ * /api/zones/{id}:
+ *   delete:
+ *     tags: [Zones]
+ *     summary: Petugas menghapus data Laporan jalan rawan (ENDPOINT BARU 5 - Khusus Petugas)
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID nomor Laporan jalan yang ingin dihapus permanen
+ *     responses:
+ *        200:
+ *         description: Data Laporan jalan rawan berhasil dihapus dari cloud
+ *        403:
+ *         description: Akses ditolak karena kamu bukan Petugas
+ *        404:
+ *         description: Data Laporan tidak ditemukan
+ */
+router.delete("/:id", cekKunciToken, batasiHakAkses("PETUGAS"), async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const cekData = await prisma.dangerZone.findUnique({
+            where: { id: parseInt(id) }
+        });
+
+        if (!cekdata) {
+            return res.status(404).json({
+                success: false,
+                message: "Data laporan jalan tidak ditemukan untuk dihapus!"
+            });
+        }
+        await prisma.dangerZone.delete({
+            where: { id: parseInt(id) }
+        });
+
+        res.status(200).json({
+            success: true,
+            mesagge: "Sukses petugas! Data laporan jalan rawan berhasil dihapus secara permanen."
         });
     } catch (error) { next(error); }
 });
