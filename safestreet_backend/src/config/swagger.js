ddd12cac635bd1a05a8b0b1f8311764202595dd9
wara1314 <hawarahmadhani561@gmail.com>
2026-06-21 15:44:02 +0700

    fix

--- safestreet_backend/src/config/swagger.js
+++ safestreet_backend/src/config/swagger.js
@@ -1,8 +1,26 @@
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
-// Fungsi jembatan untuk menyalakan Swagger UI menggunakan spesifikasi dari app.js
-const setupSwagger = (app, swaggerSpec) => {
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "SafeStreet Backend",
+            version: "1.0.0",
+            description: "Dokumentasi API Aplikasi Keamanan Jalan Raya dan Fasilitas Umum",
+        },
+        servers: [
+            {
+                url: "https://safestreetbackend-production.up.railway.app",
+                description: "Server Utama Live di Internet"
+            }
+        ]
+    },
+    apis: ["./src/routes/authRoutes.js", "./src/routes/zoneroutes.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+const setupSwagger = (app) => {
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     console.log("Dokumentasi Swagger UI aktif di alamat /api-docs");
 };
